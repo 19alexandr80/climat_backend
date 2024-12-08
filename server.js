8d9915cc64dd7a7447f4b849a89415f593080e9e
19alexandr80 <usanka1980@gmail.com>
2024-12-08 13:14:32 +0200

    3707

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 mongoose
   .connect(process.env.DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+    app.listen(3707, () => {
+      console.log("Server running. Use our API on port: 3707");
     });
   })
   .catch((err) => {
